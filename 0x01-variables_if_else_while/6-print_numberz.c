9e000df61e2e37c6b938803ee101b3ab979d7d51
DENNISMBURUNGANGA <mburudennis01@gmail.com>
2023-07-15 13:11:27 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,7 +8,7 @@ int main(void)
 	int n;
 	int m;
 
-	for  (n = 48; n <= 102; n++)
+	for  (n = 48; n <= 57; n++)
 	{
 		putchar (n);
 	}
@@ -16,6 +16,6 @@ int main(void)
 	{
 		putchar (m);
 	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
