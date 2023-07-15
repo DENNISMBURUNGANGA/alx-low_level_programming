8219dc429359dc081e381f39a560a35128309414
DENNISMBURUNGANGA <mburudennis01@gmail.com>
2023-07-15 11:52:43 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower casd
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ld;
+
+	for (ld = 'a'; ld <= 'z'; ld++)
+	{
+		putchar (ld);
+	}
+	putchar ('\n');
+	return (0);
+}
