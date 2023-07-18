a32de5b3dc7b9eb29a99829facb322f3ea71fe8b
DENNISMBURUNGANGA <mburudennis01@gmail.com>
2023-07-18 04:18:09 -0700

    0. _putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,16 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		if (i == 0)
-			putchar (i + '0');
-		else
-		putchar (i + '0');
-		putchar (',');
-		putchar (' ');
+		 putchar (i + '0');
+
+		if (i != 8)
+		{
+			putchar (',');
+			putchar (',');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
